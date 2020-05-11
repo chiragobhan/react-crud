e56d2428caccca0a50b8d089b67dfcb7fbd3dd9b
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2020-05-11 22:02:57 +0530

    Updates

---
+++ precache-manifest.719bb304afe3298bb27103a7d11f4f50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c7e5c4c0f9c49dfd273d22e5d8c675",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "332c3f8f6bb97e966e55",
+    "url": "/react-crud/static/js/2.43c37738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crud/static/js/2.43c37738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41db991600b83c1ed014",
+    "url": "/react-crud/static/js/main.0e7ffb14.chunk.js"
+  },
+  {
+    "revision": "4ada2558021541ab4d21",
+    "url": "/react-crud/static/js/runtime-main.ef6c0c02.js"
+  }
+]);
